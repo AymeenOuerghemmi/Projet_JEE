6e9d2d018e68dde4067fa1f38eb4cefe920c7329
Aymen Ouerghemmi <159343354+AymeenOuerghemmi@users.noreply.github.com>
2024-05-31 22:03:49 +0100

    Update SingletonConnection.java

--- src/main/java/tn/iit/metier/SingletonConnection.java
+++ src/main/java/tn/iit/metier/SingletonConnection.java
@@ -1,25 +1,30 @@
 package tn.iit.metier;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class SingletonConnection {
-	private static Connection connection;
-	
-	static {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			connection= DriverManager.getConnection
-					("jdbc:mysql://localhost:3306/projet_jee","root","password");
-		} catch (Exception e) {
-			
-			e.printStackTrace();
-		}
-			
-	}
+    private static Connection connection;
+    
+    static {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/projet_jee?useSSL=false&serverTimezone=UTC", 
+                "root", 
+                "password"
+            );
+        } catch (ClassNotFoundException e) {
+            System.err.println("MySQL JDBC Driver not found.");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.err.println("Connection to database failed.");
+            e.printStackTrace();
+        }
+    }
 
-	public static Connection getConnection() {
-		return connection;
-	}	
+    public static Connection getConnection() {
+        return connection;
+    }
 }
